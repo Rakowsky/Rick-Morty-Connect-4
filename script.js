8ac7aade32904b221b177d81f77eb4e89637798e
Amanda Munch <amandajmunch@gmail.com>
2017-02-16 10:05:29 -0500

    adjusting gameboard

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
 
-// /create board for 6x7
+// /create board for 6x7 connect 4
 
 var row = [6];
 var column = [7];
 var boardCircles = [];
+var characters = "abcdefg";
 
+function createBoard(){
    for (var i = 0; i < row; i++) {
         // var $slotRow = $('<div>');
         // $slotRow.addClass('slotRow');
         // $('.gameboard').append($slotRow)
       for (var j = 0; j < column; j++) {
            var $slot = $('<div>');
-           $slot.addClass('slot');
+           $slot.addClass('slot ' + (i+1));
            $('.gameboard').append($slot);
       }
    }
+}
+
+createBoard();
+
+
 
 // //setting players
 // var player1 = "player1";
