09e945222f67464c307d8551647f21226e1d48e1
Amanda Munch <amandajmunch@gmail.com>
2017-02-16 16:39:55 -0500

    created backend of board

--- script.js
+++ script.js
@@ -1,71 +1,70 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-    var row = 6;
-    var column = 7;
-    var $board = $('.gameboard');
+var row = 6;
+var column = 7;
 
-    // /create board for 6x7 connect 4
-    function createBoard() {
-        for (var i = 0; i < row; i++) {
-            for (var j = 0; j < column; j++) {
-                var $slot = $('<div>');
-                // adding individual class names to each slot
-                $slot.addClass('slot')
-                $slot.addClass('column ' + j);
-                $slot.addClass('row ' + i)
-                $board.append($slot);
-
-            }
-        }
-    }
-
-    createBoard();
-
+var board = [];
+// /create board for 6x7 connect 4
+function createBoard(){
+   for (var i = 0; i < row; i++) {
+    board[i] = [];
+      for (var j = 0; j < column; j++) {
+            var $slot = $('<div>');
+            // adding individual class names to each slot
+            $slot.addClass('slot');
+            $slot.addClass('column ' + j);
+            $slot.addClass('row ' + i);
+            $('.gameboard').append($slot);
+            board[i][j] = 0;
+      }
+   }
+}
 
 
-    // //setting players
-    // var player1 = "player1";
-    // var player2 = "player2";
+createBoard();
 
-    // var turn = ""; turn count 1
+console.table(board);
 
-    var checkForWinner = function(row, column) {
-        // pseudocode
+// //setting players
+// var player1 = "player1";
+// var player2 = "player2";
 
-    };
+// var turn = ""; turn count 1
 
-    // checkForWinner();
+var checkForWinner = function(row, column){
 
-    //player 1 red turn
-    var $player1 = $(function() {
-        $('.slot').hover(
-            function() {
-                $(this).addClass('red')
-            },
-            function() {
-                $(this).removeClass('red')
-            });
-    })
-    $('.slot').click(function(event) {
-        $(this).off().addClass('red');
-        checkForWinner();
+};
 
+// checkForWinner();
+
+//player 1 red turn
+var $player1 = $(function() {
+    $('.slot').hover(
+    function() {
+        $(this).addClass('red');
+    }, function() {
+        $(this).removeClass('red');
     });
-
-    // player 2 yellow turn
-    var $player2 = $(function() {
-        $('.slot').hover(
-            function() {
-                $(this).addClass('yellow')
-            },
-            function() {
-                $(this).removeClass('yellow')
-            });
-    })
+});
     $('.slot').click(function(event) {
-        $(this).off().addClass('yellow');
-        checkForWinner();
+          $(this).off().addClass('red');
+          checkForWinner();
+
+});
+
+// player 2 yellow turn
+var $player2 = $(function() {
+    $('.slot').hover(
+    function() {
+        $(this).addClass('yellow');
+    }, function() {
+        $(this).removeClass('yellow');
     });
+});
+    $('.slot').click(function(event) {
+          $(this).off().addClass('yellow');
+         checkForWinner();
+});
 
 
 
