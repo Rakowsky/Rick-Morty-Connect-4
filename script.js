b712a2342a3b367c2b3c3d4c25293deb9325fb3f
Amanda Munch <amandajmunch@gmail.com>
2017-02-17 11:35:03 -0500

    working on looping through classnames

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 
 var row = 6;
 var column = 7;
-
+var index = 0;
 var board = [], i=0;
+
 // var boardLog = [];
 // /create board for 6x7 connect 4
 function createBoard(){
@@ -14,13 +15,8 @@ function createBoard(){
             // adding individual class names to each slot
             $slot.addClass('slot');
             $slot.addClass('column' + j);
-            $slot.addClass(j);
             $slot.addClass('row' + i);
-            $slot.addClass(i);
-            // console.log(`row ${i}`)
-            // console.log(`column ${j}`)
             $('.gameboard').append($slot);
-            // boardLog.push($slot);
             board[i][j] = 0;
       }
    }
@@ -42,27 +38,32 @@ console.log(board.length);
 // pseudocode
 // when a div slot is clicked, adds 1 or 2 to board array
 
-var checkForWinner = function(row,column){
+var checkForWinner = function(){
   // check columns
+  var column = $('.column' + i);
+  var row = $('.row');
 
-  for(var j = 0; j<$('.column').length; j++) {
-    if($('.yellow').length>3){
-      console.log('yellow wins!');
+  // console.log(column);
+  // console.log(row);
+
+  for(var j = 0; j<column.length; j++) {
+    if($('.yellow').length === 4){
+      alert('yellow wins!');
     }
-        else if($('.red').length>3){
-          console.log('red wins!');
+        else if($('.red').length === 4){
+          alert('red wins!');
         } else{
           console.log('hi');
         }
 
 
     //test
-  } for(var i = 0; i<$('.row').length; i++) {
-    if($('.yellow').length>3){
-      console.log('yellow wins!');
+  } for(var i = 0; i<row.length; i++) {
+    if($('.yellow').length === 4){
+      alert('yellow wins!');
     }
-        else if($('.red').length>3){
-          console.log('red wins!');
+        else if($('.red').length === 4){
+          alert('red wins!');
         } else{
           console.log('hi');
         }
@@ -70,22 +71,10 @@ var checkForWinner = function(row,column){
 
 
   }
-  console.log($('.column[i]'));
+  // console.log(column.children());
+    // total += parseInt( column[i] )
 };
-//   var $row = $('.row');
-//   var $column = $('.column');
-
-// for($row = 0; $row < $row.length; $row++){
-//   for($column = 0; $column < $column.length; $column++){
-//     if($('.yellow')>3){
-//       alert('youwin');
-//       console.log('hi');
-//     } else {
-//       console.log('bye');
-//     }
-//   }
-// }
-// };
+;
 
 checkForWinner();
 
@@ -119,7 +108,7 @@ var $player2 = $(function() {
       // Taka wrote some stuff here but it was a mess and didn't work
       // console.log(this, event);
       // console.log(event.target.classList);
-      // let yes = event.target.classList;
+      let yes = event.target.classList;
       // let newXIndex = 0;
       // let newYIndex = 0;
       // yes.forEach((ye, i) => {
@@ -140,15 +129,12 @@ var $player2 = $(function() {
       // }
       // debugger
       $(this).off().addClass('yellow');
-
+         checkForWinner();
       // board.splice(i,1,2);
       // board[parseInt(event.target.classList[2])][parseInt(event.target.classList[4])] = 2;
       // console.table(board);
-      checkForWinner();
+
 });
-
-
-
 
 
 
